6fd4fc9100019d0dca63a27e6548c1c85ce3fc79
Daria Jung <djj2115cu@gmail.com>
2014-06-17 15:16:33 -0400

    set up route on flask backend to take url

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import (Flask, Response, request, render_template)
 import shorten
+import redis
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ app.debug = True
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/shorten', methods=['POST'])
+def return_shortened():
+    url_to_parse = request.form.get('url_to_parse')
+    print url_to_parse
+
+    return "OK"
+
 
 if __name__ == '__main__':
     app.run()
---
+++ static/assets/js/home.js
@@ -0,0 +1,1 @@
+var shortenerApp = angular.module('shortenerApp', []);
